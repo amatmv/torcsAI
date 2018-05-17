c4c5a0ac054147cd0a82b7619785e113a1d20d6a
Amat Martínez Vilà <amatmv@gmail.com>
2018-05-17 16:56:57 +0200

    Improvements

--- src/scr/DeadSimpleSoloController.java
+++ src/scr/DeadSimpleSoloController.java
@@ -13,10 +13,10 @@ import static java.lang.Math.abs;
  */
 public class DeadSimpleSoloController extends Controller {
 
-    private final double _targetSpeed = 50;
-    private ArrayList<Double> _trackAngles = new ArrayList<Double>();
-    private ArrayList<Double> _trackAnglesSummarized = new ArrayList<Double>();
-    private int _trackDistance = 0;
+    private final double _targetSpeed = 40;
+    private ArrayList<Double> _trackAngles = new ArrayList<>();
+    private ArrayList<Double> _auxTrackAngles = new ArrayList<>();
+    private ArrayList<Double> _trackAnglesSummarized = new ArrayList<>();
     private double _laps = 0;
 
     public Action control(SensorModel sensorModel) {
@@ -31,20 +31,19 @@ public class DeadSimpleSoloController extends Controller {
         }
 
         if (_laps <= 1) {
-            _trackDistance = (int) sensorModel.getDistanceFromStartLine();
             getTrackInfo(sensorModel);
         }
 
-        if (sensorModel.getAngleToTrackAxis() < -0.02) { // Negative angles are left angles
-            // Girar a la dreta al màxim
-            action.steering = -0.5;
-        } else if (sensorModel.getTrackPosition() > 1/5.0) { // Negative distances means "nearer to left edge"
-            action.steering = -0.15;
-        } else if (sensorModel.getAngleToTrackAxis() > 0.02) { // Positive angles means that we are deviating to the right
-            // Gira el màxim a l'esquerra
-            action.steering = 0.5;
-        } else if (sensorModel.getTrackPosition() < -1/5.0) { // Positive distances means "nearer to right edge"
-            action.steering = 0.15;
+        if (sensorModel.getAngleToTrackAxis() < -0.15) { // Negative angles are left angles
+            // Girar a la dreta
+            action.steering = -1;
+        } else if (sensorModel.getTrackPosition() > 1/50.0) { // Negative distances means "nearer to left edge"
+            action.steering = -0.25;
+        } else if (sensorModel.getAngleToTrackAxis() > 0.15) { // Positive angles means that we are deviating to the right
+            // Gira a l'esquerra
+            action.steering = 1;
+        } else if (sensorModel.getTrackPosition() < -1/50.0) { // Positive distances means "nearer to right edge"
+            action.steering = 0.25;
         }
 
         action.gear = 1;
@@ -53,21 +52,22 @@ public class DeadSimpleSoloController extends Controller {
 
     private void getTrackInfo(SensorModel sensorModel) {
 
-        final int meanFactor = 100;
+        final int meanFactor = 30;
 
         double currentAngle = sensorModel.getAngleToTrackAxis();
-        _trackAngles.add(currentAngle);
+        _auxTrackAngles.add(currentAngle);
 
-        int currentTrackSize = _trackAngles.size();
+        int currentTrackSize = _auxTrackAngles.size();
 
-        if (currentTrackSize > 0 && currentTrackSize % meanFactor == 0){
+        if (currentTrackSize >= meanFactor){
 
             double meanAngle = 0;
 
-            for (int i = currentTrackSize-1; i >= currentTrackSize-meanFactor; i--){
-                meanAngle += _trackAngles.get(i);
+            for (int i = currentTrackSize-1; i >= 0; i--){
+                meanAngle += _auxTrackAngles.remove(i);
             }
-            _trackAngles.add(currentTrackSize-meanFactor+1, meanAngle /= meanFactor);
+
+            _trackAngles.add(meanAngle / meanFactor);
 
         }
 
@@ -78,6 +78,21 @@ public class DeadSimpleSoloController extends Controller {
     }
 
     public void shutdown() {
+        int size = _trackAngles.size();
+
+        int j = -1;
+        for (int i = 0; i < size; i++) {
+            double value = _trackAngles.get(i);
+            if (i % 2 == 0)
+                _trackAnglesSummarized.add(++j, value);
+            else
+                _trackAnglesSummarized.set(j, (_trackAnglesSummarized.get(j) + value) * 100);
+        }
+        System.out.println("tamany vector angles" + size);
+        System.out.println("Valors del vector final" + _trackAnglesSummarized.size());
+        for (double value: _trackAnglesSummarized) {
+            System.out.println(value);
+        }
         System.out.println("Bye bye!");		
     }
 }
