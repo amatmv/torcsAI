2d79c601f50320277ca33812e03b0d1aa1f029f7
polsala <polsalalidon@gmail.com>
2018-05-26 18:13:47 +0200

    Funcio estabilitzadora.

--- src/scr/InferenceController.java
+++ src/scr/InferenceController.java
@@ -140,6 +140,11 @@ public class InferenceController extends Controller {
             System.out.println("-----------------------------------------------------------------");
         }
 
+        estabilitzar(sensorModel, action, gir, acceleracio, fre, angleCurva);
+
+        hack(action, metreActual);
+
+
         return action;
     }
 
@@ -151,6 +156,120 @@ public class InferenceController extends Controller {
         }
         return angleAcumulat / 12.892601299999997;
     }
+
+    /************************** Funcions correctives ********************************/
+    private static boolean foraDePista(double trackPos){
+        return trackPos > 1.0 || trackPos < - 1.0;
+    }
+
+    private double calcularDireccio(double[] trackEdges){
+        double forwardDist = 0;
+        int direction = 0;
+        for (int i = 7; i < 12; ++i) {
+            if (trackEdges[i] > forwardDist) {
+                forwardDist = trackEdges[i];
+                direction = 9 - i;
+            }
+        }
+        return direction;
+    }
+
+    private double calcularAngleSegonsDireccio(double direccio, double trackPos){
+        if (direccio == 0) {
+            return 0.1 * (trackPos - 0.5);
+        }
+        if (direccio == 1) {
+            return 1.1 * (trackPos - 0.7);
+        }
+        if (direccio == -1) {
+            return 1.1 * (trackPos - 0.3);
+        }
+        if (direccio == 2) {
+            return 1.1 * (trackPos - 0.8);
+        }
+        if (direccio == -2) {
+            return 1.1 * (trackPos - 0.2);
+        }
+        return 0;
+    }
+
+    private void estabilitzar(SensorModel sensorModel, Action action, double gir, double acceleracio, double fre, double angleCurva){
+        double trackPos = sensorModel.getTrackPosition(); // indica la posicio respecte els vorals
+        double trackAxis = sensorModel.getAngleToTrackAxis();
+        double velocity = sensorModel.getSpeed();
+        double[] trackEdges = sensorModel.getTrackEdgeSensors();
+        double direccio = calcularDireccio(trackEdges);
+
+        if (!foraDePista(trackPos)){
+            if (Math.abs(angleCurva) >0.7) {
+                if (gir > 0.4) {
+                    action.steering = action.steering - 0.07;
+                } else if (gir < -0.4) {
+                    action.steering = action.steering + 0.07;
+                }
+            }
+
+            if(Math.abs(trackPos) > 0.80){
+                if (trackPos < 0){ //dreta
+                    if (gir < 0){
+                        action.steering = action.steering + -(action.steering/4);
+                    }
+                }else {
+                    if (gir > 0){
+                        action.steering = action.steering -(action.steering/4);
+                    }
+                }
+            }
+        }else{
+            System.out.println("jesus");
+            System.out.println(trackPos);
+            if (acceleracio > 0.4){
+                action.accelerate = 0.35;
+            }
+            if (velocity < 50){
+                action.gear = 1;
+            }
+            if (velocity < 0.0 && velocity >= -10.0){
+                action.gear = -1;
+            }
+            if (trackPos < 0){ //esq
+                if (gir < 0){
+                    action.steering = -1 * action.steering;//- * - = +
+                }
+            }else{
+                if (gir > 0){
+                    action.steering = -1 * action.steering;
+                }
+            }
+
+        }
+
+
+
+    }
+
+    private void hack(Action action, int metre){
+
+        //if (metre > 2325 & metre < 2450){
+        if (metre > 2325 & metre < 2375){
+            System.out.println("Hack life");
+            action.accelerate = 0.2;
+            action.brake = 0.0;
+        }
+        if (metre > 3830 & metre < 3870){
+            System.out.println("Hack life2");
+            action.accelerate = 0.05;
+            if (Math.abs(action.steering)> 0.4){
+                if (action.steering > 0){
+                    action.steering= - 0.35;
+                }else{
+                    action.steering= + 0.2;
+                }
+
+            }
+        }
+    }
+    /******************************************************************/
 
 
     public void reset() {
